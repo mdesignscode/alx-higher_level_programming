46e5600a8e7f22d623097c8ba64d2f82a93bb855
Mdesigns Code <marlonbaatjes830@gmail.com>
2022-05-05 05:28:09 -0700

    Reading and Writing Files

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Add all arguments to a Python list, and then save them to a file"""
 sys = __import__('sys')
-load_from_json_file = __import__(6-load_from_json_file).load_from_json_file
-save_to_json_file = __import__(5-save_to_json_file).save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 if __name__ == '__main__':
