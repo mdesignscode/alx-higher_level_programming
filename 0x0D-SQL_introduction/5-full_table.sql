3276efc5219fca4f15aa407d69825856adea2d38
marlon <marlon@debian-BULLSEYE-live-builder-AMD64>
2022-08-18 00:00:48 +0200

    table decription correction

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
 -- prints the full description of the table first_table
 -- from the database passed as argument
 
-SELECT
-'Table   Create Table
-first_table     CREATE TABLE `first_table` (\n  `id` int DEFAULT NULL,\n  `name` varchar(256) DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' AS '';
+SHOW CREATE TABLE first_table;
