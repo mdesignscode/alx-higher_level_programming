d8033e04dc94f6b1b65cff7f1f46fa8222fa6bc2
Mdesigns Code <marlonbaatjes62@gmail.com>
2022-11-11 20:08:51 +0200

    🔧🔨🌈💅💄 fix semicode styling 🔧🔨🌈💅💄

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,11 +5,11 @@ const url = process.argv.slice(2)[0];
 
 request(url, (error, response) => {
   if (!error) {
-    const listOfCompleted = JSON.parse(response.body).filter((a) => { return a.completed});
+    const listOfCompleted = JSON.parse(response.body).filter((a) => { return a.completed; });
     const completed = {};
     for (const user of listOfCompleted) {
-      const userCompleted = listOfCompleted.filter((u) => { return u.userId ===  user.userId})
-      completed[user.userId] = userCompleted.length
+      const userCompleted = listOfCompleted.filter((u) => { return u.userId === user.userId; });
+      completed[user.userId] = userCompleted.length;
     }
     console.log(completed);
   }
